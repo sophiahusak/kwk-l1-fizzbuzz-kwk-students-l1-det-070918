76fa065714067e7fc59e6af0dd7a6003d24a73c7
sophiahusak <72futbolplayer@gmail.com>
2018-07-11 15:30:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizz_buzz(number)
-  if number % 3
+  if number % 3 == 0
     puts "Fizz"
-  elsif number % 5
+  elsif number % 5 == 0
     puts "Buzz"
-  else number % 3 && number % 5
+  else number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
   end
 end
